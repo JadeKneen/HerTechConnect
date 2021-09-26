3b879bb18554c8195fb42824ed1b88e57a73ad11
Jade Kneen <48185660+JadeKneen@users.noreply.github.com>
2021-09-26 20:53:45 +0100

    Updated CSS

--- src/BeforeLaunch/MissionStatement.js
+++ src/BeforeLaunch/MissionStatement.js
@@ -3,19 +3,17 @@ import './MissionStatement.css'
 function MissionStatement() {
     return (
       <div className="MissionStatement">
-          <div className="container">
-                <div className="border-quote">
-                    <figure class="quote">
-                        <blockquote>
-                        A mentor empowers a person to see a possible future, 
-                        and believe it can be obtained.
-                        </blockquote>
-                        <figcaption>
-                            &mdash; Shawn Hitchcock
-                        </figcaption>
-                    </figure>
-                </div>
-          </div>
+            <div className="border-quote">
+                <figure class="quote">
+                    <blockquote>
+                    A mentor empowers a person to see a possible future, 
+                    and believe it can be obtained.
+                    </blockquote>
+                    <figcaption>
+                        &mdash; Shawn Hitchcock
+                    </figcaption>
+                </figure>
+            </div>
       </div>
     );
   }
